79ea512bb932967efd7fb1463fbd202746e15612
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2021-08-28 17:43:36 +0900

    Parse command line arguments and show filename

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use bzip2::read::BzDecoder;
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args).unwrap_or_else(|err| {
+        println!("{}", err);
+        process::exit(1);
+    });
+    println!("{}", config.filename);
+}
+
+struct Config {
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Result<Config, &'static str> {
+    if args.len() < 2 {
+        return Err("not enough arguments");
+    }
+    let filename = args[1].clone();
+    Ok(Config { filename })
 }
