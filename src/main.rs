6ab58123b8d23aa352e30153f36e58820c1f3ff3
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2021-08-30 18:27:24 +0900

    Add read_mediawiki_doc()

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use bzip2::read::BzDecoder;
+use mediawiki_parser;
+use serde_yaml;
 use std::env;
 use std::error::Error;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufReader, Read};
 use xml::reader::{EventReader, XmlEvent};
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -11,29 +13,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("{}", config.filename);
 
     let reader = BufReader::new(File::open(config.filename)?);
-    let reader = BzDecoder::new(reader);
-    let parser = EventReader::new(reader);
-    let mut title_mode = false;
-    for ev in parser {
-        match ev? {
-            XmlEvent::StartElement { name, .. } => {
-                if name.local_name == "title" {
-                    title_mode = true
-                }
-            }
-            XmlEvent::EndElement { name, .. } => {
-                if name.local_name == "title" {
-                    title_mode = false
-                }
-            }
-            XmlEvent::Characters(text) => {
-                if title_mode {
-                    println!("{}", text)
-                }
-            }
-            _ => {}
-        }
-    }
+    read_mediawiki_doc(BzDecoder::new(reader))?;
     Ok(())
 }
 
@@ -47,4 +27,40 @@ fn parse_config(args: &[String]) -> Result<Config, &'static str> {
     }
     let filename = args[1].clone();
     Ok(Config { filename })
+}
+
+#[derive(PartialEq)]
+enum ReaderState {
+    Base,
+    Title,
+    Body,
+}
+
+fn read_mediawiki_doc<R: Read>(reader: R) -> Result<(), Box<dyn Error>> {
+    let parser = EventReader::new(reader);
+
+    let mut state = ReaderState::Base;
+    for ev in parser {
+        match ev? {
+            XmlEvent::StartElement { name, .. } => match name.local_name.as_str() {
+                "title" => state = ReaderState::Title,
+                "text" => state = ReaderState::Body,
+                _ => {}
+            },
+            XmlEvent::EndElement { name, .. } => match name.local_name.as_str() {
+                "title" | "text" => state = ReaderState::Base,
+                _ => {}
+            },
+            XmlEvent::Characters(text) => {
+                if state == ReaderState::Title {
+                    println!("{}", text)
+                } else if state == ReaderState::Body {
+                    let body = mediawiki_parser::parse(&text)?;
+                    println!("{}", &serde_yaml::to_string(&body)?)
+                }
+            }
+            _ => {}
+        }
+    }
+    Ok(())
 }
